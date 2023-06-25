663a4b9970561f260001c6b471403a55d87c973d
MarianneSyntax <113111255+MarianneSyntax@users.noreply.github.com>
2023-06-25 12:58:43 +0100

    Initial commit: W01

---
+++ src/W01/D05-Wiederholung/Tag01.kt
@@ -0,0 +1,17 @@
+fun main() {
+    //<editor-fold desc="main function">
+
+    // Beispiel Kompilierfehler:
+    //var x: String = 13
+
+    // Beispiel Laufzeitfehler
+    //    var list = listOf(1, 2, 3, 4)
+    //    println(list[5])
+
+    // println() und readln()
+    println("Bitte dein Alter eingeben.")
+    var alter: String = readln()
+    println("Okay, du bist also $alter Jahre alt!")
+//</editor-fold>
+}
+
---
+++ src/W01/D05-Wiederholung/Tag03.kt
@@ -0,0 +1,21 @@
+fun main(){
+
+    // Mathematische Operationen:
+    var intSum = 30+70
+    var doubleDiff = 10.5-7.5
+    var product = 8*8
+    var divident = 15.0/3.0
+    var rest = 90%20
+
+    // Type Conversion
+    var stringValue: String = readln() // "30" -> String, kann man nicht mit rechnen
+
+    var convertedString = stringValue.toDouble() // String in eine Zahl, nämlich Double, umwandeln
+    println("${stringValue+5}") // String + 5
+    println("ConvertedString (also ein Double) +5: ${convertedString+5}")
+
+    var doubleValue = convertedString
+    var convertedDouble = convertedString.toInt()
+
+
+}

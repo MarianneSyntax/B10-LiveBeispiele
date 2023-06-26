e9d22d5925a98eaf94f5069d78ae8a0a7fc5e75c
MarianneSyntax <113111255+MarianneSyntax@users.noreply.github.com>
2023-06-26 10:29:32 +0100

    wiederholung

---
+++ src/W02/D01/Tag06-Wiederholung-W01.kt
@@ -0,0 +1,12 @@
+fun main(){
+
+    // Skill Check - Aufgabe
+    var name1: String = "Herbert"
+    var name2: String = "Maria"
+
+    name1 = name2 // wo vorher Herbert drin stand, steht jetzt Maria (der Inhalt von name2) drin
+
+    println("Bist du $name1?") // hier mal einen Breakpoint setzen und den Debugger (Käfer-Symbol neben dem Play Symbol oben rechts) nutzen :)
+    println("Der Code ist gestoppt, bevor wir in diese Zeile gekommen sind.")
+
+}

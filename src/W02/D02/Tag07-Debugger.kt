7ae078725c3d223220f55d53afe900adcaa0d2ac
MarianneSyntax <113111255+MarianneSyntax@users.noreply.github.com>
2023-06-28 09:34:36 +0100

    Tag 7: Debugger, kommentar klarer

--- src/W02/D02/Tag07-Debugger.kt
+++ src/W02/D02/Tag07-Debugger.kt
@@ -4,10 +4,10 @@ fun main() {
 
     var numbers: List<Int> = listOf(98,347,50,21,57,77,84,31)
     // diese Zeile:
-    println(numbers[numbers.size-1])
+    println(numbers[numbers.size-1]) // -1 fehlte hier vorher
     // ist das gleiche wie:
-    println(numbers[7])
-    // die Liste geht aber nur bis Index 7. Hier wird eine IndexOutOfBoundsException getriggert.
+    println(numbers[7]) // hier stand vorher eine 8 statt der 7 in den []
+    // die Liste geht aber nur bis Index 7. Hier wird eine IndexOutOfBoundsException getriggert, wenn wir nicht -1 auf size verwenden.
 
     // Werte der Variablen mit Debugger auslesen
     var number1 = 5

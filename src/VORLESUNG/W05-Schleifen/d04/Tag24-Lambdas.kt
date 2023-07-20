286a15580b68c4491ef7065e6d8995a7f5a6a505
MarianneSyntax <113111255+MarianneSyntax@users.noreply.github.com>
2023-07-20 10:11:10 +0100

    LiveBeispiele Tag24: in Lambdas indent gefixt, bessere Kommentare

--- src/VORLESUNG/W05-Schleifen/d04/Tag24-Lambdas.kt
+++ src/VORLESUNG/W05-Schleifen/d04/Tag24-Lambdas.kt
@@ -5,41 +5,49 @@ package VORLESUNG.`W05-Schleifen`.d04
 
 fun main() {
 
-        // greaterFive als Funktion
-        fun greaterFiveFun(nr: Int): Boolean {
-            val result = nr > 5
-            println("$nr is greater than 5? -> $result")
-            return result
-        }
-    greaterFiveFun(7)
+    // greaterFive: normale Funktion
+    fun greaterFiveFun(nr: Int): Boolean {
+        val result = nr > 5
+        println("$nr is greater than 5? -> $result")
+        return result
+    }
+    // Aufruf von greaterFive
+    greaterFiveFun(7) // true
 
-         // Gegenüberstellung:
-        // greaterFour als Lambda
-        var greaterFour = { nr:Int -> (nr>4) }
-        var x = greaterFour(readln().toInt())
-        var y = greaterFour(9) // true
+     // Gegenüberstellung:
+    // greaterFour als Lambda
+    // Lambda kann in einer Variable gespeichert werden:
+    var greaterFour = { nr:Int -> (nr>4) }
 
-        var addNrWith2 = {nr:Int -> nr+2}
+    // diese Variable kann dann wie eine Funktion verwendet werden:
+    println("Bitte Int für greaterFour Aufruf eingeben")
+    var x = greaterFour(readln().toInt()) // readln(), sodass Zahl eingegeben werden muss
+    var y = greaterFour(9) // Zahl direkt als Argument übergeben
 
-        var zweiPlusZwei = addNrWith2(2)
-        var dreiPlusZwei = addNrWith2(3)
+    // auch hier: Lambda in Variable speichern
+    var addNrWith2 = {nr:Int -> nr+2}
 
-
-         val planets = listOf("Mein ", "Vater ", "erklaert ", "mir ", "jeden ", "Sonntag ", "unsere ", "neun ", "Planeten")
-       // Planeten Liste mit for-Schleife durchgehen
-        for (planet in planets){
-            print("$planet ")
-        }
-        println("")
+    // mit dieser Variable dann aufrufen
+    var zweiPlusZwei = addNrWith2(2)
+    var dreiPlusZwei = addNrWith2(3)
 
 
-        // Gegenüberstellung:
-        // Planeten Liste mit forEach Lambda
-        // kurze Syntax mit it: Parameter kann weggelassen werden, it ist Schlüsselwort für Element
-        planets.forEach { print("$it ") }
-        println("")
-        // längere Syntax: Parameter mit eigenem Parameternamen: muss angegeben werden
-        planets.forEach{ planet -> print("$planet ") }
+     val planets = listOf("Mein ", "Vater ", "erklaert ", "mir ", "jeden ", "Sonntag ", "unsere ", "neun ", "Planeten")
+    // Planeten Liste mit for-Schleife durchgehen
+    for (planet in planets){
+        print("$planet ")
+    }
+    println("")
+
+
+    // Gegenüberstellung:
+
+    // Planeten Liste mit forEach Lambda:
+    // kurze Syntax mit it: Parameter kann weggelassen werden, it ist Schlüsselwort für das jeweilige Element
+    planets.forEach { print("$it ") }
+    println("")
+    // längere Syntax: Parameter mit eigenem Parameternamen: muss dann mit name -> vor der Operation angegeben werden
+    planets.forEach{ planet -> print("$planet ") }
 
 }
 
